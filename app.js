5bbdebae7fedd6eab60d0d055499cba180b54313
saradchhetri07 <saradchhetri20690@gmail.com>
2023-02-22 23:47:43 +0545

    levels 4 of security,authentication and use of salting and hasing rounds to add security strength

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 const bodyParser = require("body-parser");
-var md5 = require("md5");
+// var md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 main().catch((err) => console.log(err));
 
@@ -56,19 +58,55 @@ app.get("/logout", function (req, res) {
   res.redirect("/");
 });
 
+// app.post("/register", function (req, res) {
+//   const formUsername = req.body.username;
+//   const formPassword = req.body.password;
+
+//   const newUser = new Users({
+//     Username: formUsername,
+//     Password: md5(formPassword),
+//   });
+
+//   newUser.save(function (err) {
+//     if (err) {
+//       console.log(err);
+//     }
+//   });
+//   res.redirect("secrets");
+// });
+
 app.post("/register", function (req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    // Store hash in your password DB.
+    if (!err) {
+      const formUsername = req.body.username;
+      const newUser = new Users({
+        Username: formUsername,
+        Password: hash,
+      });
+      newUser.save();
+    } else {
+      res.send("error while hashing");
+    }
+  });
+  res.redirect("/");
+});
+
+app.post("/login", function (req, res) {
   const formUsername = req.body.username;
   const formPassword = req.body.password;
 
-  const newUser = new Users({
-    Username: formUsername,
-    Password: md5(formPassword),
-  });
-
-  newUser.save(function (err) {
+  Users.findOne({ Username: formUsername }, function (err, foundUser) {
     if (err) {
-      console.log(err);
+    } else {
+      bcrypt.compare(formPassword, foundUser.Password, function (err, results) {
+        // result == false
+        if (results === true) {
+          res.render("secrets");
+        } else {
+          res.send("not a valid user");
+        }
+      });
     }
   });
-  res.redirect("secrets");
 });
